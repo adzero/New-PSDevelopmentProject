9d24d5c598789e5a123a16127aa7296b8bc2622c
AdZero <7522988+adzero@users.noreply.github.com>
2018-06-16 18:11:03 +0200

    NEW: Version 1.0.0
    Signed-off-by: AdZero <7522988+adzero@users.noreply.github.com>

---
+++ tests/New-PSDevelopmentProject.Tests.ps1
@@ -0,0 +1,313 @@
+﻿
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID abb5153e-e27f-4f7e-8e5b-b85ccefd1e0e
+
+.AUTHOR AdZero
+
+.COMPANYNAME
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Tests for New-PSDevelopmentProject project 
+
+#> 
+Param()
+
+
+
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 00fe9040-1bc7-4862-9e45-e0a59c005d65
+
+.AUTHOR AdZero
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Tests for New-PSDevelopmentProject project 
+
+#> 
+$here = (Get-Item (Split-Path -Parent $MyInvocation.MyCommand.Path)).Parent.FullName
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$name = (Get-Item (Split-Path -Parent $MyInvocation.MyCommand.Path)).Parent.Name
+
+$Script:scriptPath = "$here\$name\$sut"
+$Script:testPath = Join-Path -Path $env:TEMP -ChildPath ([Guid]::NewGuid().ToString())
+
+Describe "New-PSDevelopmentProject | Test script project creation" {
+
+	Context "Without GIT repository" {
+
+		It "Check script projects root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project License file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\LICENSE") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test script project Markdown README file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\README.md") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test script project code directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project code directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\TestScriptProject.ps1") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\TestScriptProject.Format.ps1xml") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\lib") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\bin") -PathType Container | Should -Be $true
+		}
+
+		It "Check test script project Pester tests directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\Tests") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project Pester tests directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\Tests\TestScriptProject.Tests.ps1") -PathType Leaf | Should -Be $true
+		}
+		
+		BeforeAll {
+
+			New-Item -Path $Script:testPath -ItemType Directory
+			&"$($Script:scriptPath)" -Name TestScriptProject -Author Pester -Description "PowerShell test script" -RootPath $testPath -ScriptProject
+		}
+		
+		AfterAll {
+			
+			Remove-Item -Path $Script:testPath -Recurse -Force -ErrorAction SilentlyContinue
+		
+		}
+	}
+	
+	Context "With GIT repository" {
+
+		It "Check script projects root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project License file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\LICENSE") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test script project Markdown README file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\README.md") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test script project code directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project code directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\TestScriptProject.ps1") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\TestScriptProject.Format.ps1xml") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\lib") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\TestScriptProject\bin") -PathType Container | Should -Be $true
+		}
+
+		It "Check test script project Pester tests directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\Tests") -PathType Container| Should -Be $true
+		}
+
+		It "Check test script project Pester tests directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\Tests\TestScriptProject.Tests.ps1") -PathType Leaf | Should -Be $true
+		}
+		
+		It "Check test script project git repository directory" {
+			#Better tests needed...
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Scripts\TestScriptProject\.git") -PathType Container| Should -Be $true
+		}
+		
+		BeforeAll {
+
+			New-Item -Path $Script:testPath -ItemType Directory
+			&"$($Script:scriptPath)" -Name TestScriptProject -Author Pester -Description "PowerShell test script" -RootPath $testPath -ScriptProject -GitRepository
+		}
+		
+		AfterAll {
+			
+			Remove-Item -Path $Script:testPath -Recurse -Force -ErrorAction SilentlyContinue
+		
+		}
+	}
+}
+
+Describe "New-PSDevelopmentProject | Test module project creation" {
+
+	Context "Without GIT repository" {
+
+		It "Check module projects root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project License file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\LICENSE") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test module project Markdown README file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\README.md") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test module project code directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project code directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\TestModuleProject.psm1") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\TestModuleProject.psd1") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\TestModuleProject.Format.ps1xml") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\Public") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\Private") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\lib") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\bin") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\en-US") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\en-US\about_TestModuleProject.help.txt") -PathType Leaf | Should -Be $true
+		}
+
+		It "Check test module project Pester tests directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\Tests") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project Pester tests directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\Tests\TestModuleProject.Tests.ps1") -PathType Leaf | Should -Be $true
+		}
+		
+		BeforeAll {
+
+			New-Item -Path $Script:testPath -ItemType Directory
+			&"$($Script:scriptPath)" -Name TestModuleProject -Author Pester -Description "PowerShell test module" -RootPath $testPath -ModuleProject
+		}
+		
+		AfterAll {
+			
+			Remove-Item -Path $Script:testPath -Recurse -Force -ErrorAction SilentlyContinue
+		
+		}
+	}
+	
+	Context "With GIT repository" {
+
+		It "Check module projects root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project root directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project License file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\LICENSE") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test module project Markdown README file" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\README.md") -PathType Leaf| Should -Be $true
+		}
+		
+		It "Check test module project code directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project code directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\TestModuleProject.psm1") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\TestModuleProject.psd1") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\TestModuleProject.Format.ps1xml") -PathType Leaf | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\Public") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\Private") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\lib") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\bin") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\en-US") -PathType Container | Should -Be $true
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\TestModuleProject\en-US\about_TestModuleProject.help.txt") -PathType Leaf | Should -Be $true
+		}
+
+		It "Check test module project Pester tests directory" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\Tests") -PathType Container| Should -Be $true
+		}
+
+		It "Check test module project Pester tests directory default content" {
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\Tests\TestModuleProject.Tests.ps1") -PathType Leaf | Should -Be $true
+		}
+		
+		It "Check test module project git repository directory" {
+			#Better tests needed...
+			Test-Path -Path (Join-Path -Path $testPath -ChildPath "Modules\TestModuleProject\.git") -PathType Container | Should -Be $true
+		}
+
+		BeforeAll {
+
+			New-Item -Path $Script:testPath -ItemType Directory
+			&"$($Script:scriptPath)" -Name TestModuleProject -Author Pester -Description "PowerShell test module" -RootPath $testPath -ModuleProject -GitRepository
+		}
+		
+		AfterAll {
+			
+			Remove-Item -Path $Script:testPath -Recurse -Force -ErrorAction SilentlyContinue
+		
+		}
+	}
+}
